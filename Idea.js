601cefa75d1352ceda92ba7ee9b36c658b7afa5d
Reid Poole <poole.reid@gmail.com>
2022-11-03 21:25:36 -0500

    Add: class property and functions, updateIdea and renderIdeaBox

--- Idea.js
+++ Idea.js
@@ -1,10 +1,13 @@
 class Idea {
-    constructor(title, body) {
-        this.title = title;
-        this.body = body;
-        this.star = true;
-    }     
-    updateIdea() {
-        
+    constructor (title, body) {
+            this.title = title
+            this.body = body
+            this.id = Date.now();
+            this.star = false;
+
     }
+            updateIdea() {
+                this.star = true; 
+            
+}
 }
--- main.js
+++ main.js
@@ -12,6 +12,42 @@ var whiteStar = document.querySelectorAll('.white-star-icon');
 var whiteX = document.querySelectorAll('.white-x-icon');
 var commentIcon = document.querySelectorAll('.comment-icon');
 
-var cardContainer = document.querySelector('.card-container')
+var cardContainer = document.querySelectorAll('.card-container')
 var cardTitle = document.querySelectorAll('card-title');
 var cardBody = document.querySelectorAll('card-body');
+
+// -----------------global variables-----------------------------------
+
+var ideaBoxArray = [ ]
+
+// ----------------eventListeners-----------------------
+
+saveButton.addEventListener('click', updateIdea)
+// ----------------functions------------------------------
+
+function updateIdea() {
+    
+    var newIdeaBox = new Idea(titleInput.value, bodyInput.value)
+    console.log(newIdeaBox)
+    ideaBoxArray.push(newIdeaBox)
+    console.log(ideaBoxArray)
+}
+
+
+
+function renderIdeaBox() {
+    cardContainer.innerHTML = ' ';
+    for (var i = 0; i < ideaBoxArray.length; i++){
+    cardContainer[i].innerHTML += `
+                <div class="card-header">
+                    <img class="red-star-icon" src="assets/star-active.svg">
+                    <img class="white-x-icon" src="assets/delete.svg">
+                </div>
+                <h2 class="card-title">${titleInput.value}</h2>
+                <p class="card-body">${bodyInput.value}</p>
+                <div class="card-footer">
+                    <img class="comment-icon" src="assets/comment.svg">
+                    <a class="comment-text"> Comment</a>
+                </div>`
+    }
+}
