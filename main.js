8e0d4235617320381a4bf454cd5b1911f6ad0fff
Reid Poole <poole.reid@gmail.com>
2022-11-03 20:33:40 -0500

    Add: querySelectors and classes to HTML

--- main.js
+++ main.js
@@ -2,11 +2,16 @@
 var sideButton = document.querySelector('.side-button');
 var saveButton = document.querySelector('.save-button');
 
+var titleInput = document.querySelector('.title-label-input');
+var bodyInput = document.querySelector('.body-input');
+var searchInput = document.querySelector('.search-bar');
+
+var searchIcon = document.querySelectorAll('.search-icon');
 var redStar = document.querySelectorAll('.red-star-icon');
 var whiteStar = document.querySelectorAll('.white-star-icon');
 var whiteX = document.querySelectorAll('.white-x-icon');
-var searchIcon = document.querySelectorAll('.search-icon');
 var commentIcon = document.querySelectorAll('.comment-icon');
 
+var cardContainer = document.querySelector('.card-container')
 var cardTitle = document.querySelectorAll('card-title');
 var cardBody = document.querySelectorAll('card-body');
