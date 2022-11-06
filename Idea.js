5f09858c4e4c9aedf32ece6b7c06dd8b5d737dce
Tristin Sorrells <Tristinsorrells1@gmail.com>
2022-11-05 22:00:27 -0500

    Fix: bug that prevented cards from being favorited

--- Idea.js
+++ Idea.js
@@ -10,15 +10,30 @@ updateIdea(event) {
                 var grandparentIdToString = parseInt(event.target.parentElement.parentElement.id)
                 if (grandparentIdToString === ideaBoxArray[i].id && event.target.classList.contains('white-star-icon')) {
                        ideaBoxArray[i].star = true
-                         console.log("star: true", ideaBoxArray)
-                }
+                         console.log("Mayb", ideaBoxArray)
+        }
                 else if (grandparentIdToString === ideaBoxArray[i].id && event.target.classList.contains('red-star-icon')) {
                         ideaBoxArray[i].star = false
-                        console.log("star: false", ideaBoxArray)
+                        console.log("better?", ideaBoxArray)
                 }
-        } 
-}
+        }
+} 
 }       
 
 
 
+
+//         updateIdea(event) {
+//                 console.log(event.target)
+//                 if (event.target.classList.contains('white-star-icon')) {
+//                         console.log("array with updated star", ideaBoxArray)
+//                         console.log("OI with updated star", newIdeaBox)
+//                         return newIdeaBox.star = true
+
+//                 }
+//                 if (event.target.classList.contains('red-star-icon')) {
+//                         return newIdeaBox.star = false;
+//                 }
+//         }
+// }  
+   
