44bd152890bb9270e526c56c5d95167fb51189ea
Tristin Sorrells <Tristinsorrells1@gmail.com>
2022-11-07 16:32:40 -0600

    Refactor: final JS, CSS, and HTML files

--- main.js
+++ main.js
@@ -1,116 +1,99 @@
-//-------------------------querySelectors------------------------
 var sideButton = document.querySelector('.side-button');
 var saveButton = document.querySelector('.save-button');
-
 var titleInput = document.querySelector('.title-label-input');
 var bodyInput = document.querySelector('.body-input');
-
 var redStar = document.querySelectorAll('.red-star-icon');
 var whiteStar = document.querySelectorAll('.white-star-icon');
 var whiteX = document.querySelectorAll('.white-x-icon');
-
 var cardContainer = document.querySelectorAll('.card-container')
 var cardTitle = document.querySelectorAll('card-title');
 var cardBody = document.querySelectorAll('card-body');
-var cardGrid = document.querySelector('.card-grid')
+var cardGrid = document.querySelector('.card-grid');
 
-// -----------------global variables-----------------------------------
+var ideaBoxArray = [ ];
+var titleInputEnteredVar = false;
+var bodyInputEnteredVar = false;
+var newIdeaBox = new Idea(titleInput.value, bodyInput.value);
 
-var ideaBoxArray = [ ]
-var titleInputEnteredVar = false
-var bodyInputEnteredVar = false
-var newIdeaBox = new Idea(titleInput.value, bodyInput.value)
-
-
-// ----------------eventListeners-----------------------
 window.addEventListener('load', disableSaveButton);
 
 titleInput.addEventListener('input', function () {
-titleInputEntered()
-enableSaveButton();
+    titleInputEntered();
+    enableSaveButton();
 });
 
 bodyInput.addEventListener('input', function () {
-bodyInputEntered()
-enableSaveButton();
+    bodyInputEntered();
+    enableSaveButton();
 });
 
 saveButton.addEventListener('click', function() {
-saveIdea()
-renderIdeaBox()
-// saveFavorites()
-disableSaveButton()
-clearForm()
+    saveIdea();
+    renderIdeaBox();
+    disableSaveButton();
+    clearForm();
 });
 
 cardGrid.addEventListener('click', function(event) {
-    event.preventDefault()
-    favoriteCard(event)
-    newIdeaBox.updateIdea(event)
-    removeFromArray(event)
-    deleteCard(event)
-    console.log(event.target.parentElement.parentElement.id)
-    
+    event.preventDefault();
+    favoriteCard(event);
+    newIdeaBox.updateIdea(event);
+    removeFromArray(event);
+    deleteCard(event);
 });
-
-// ----------------functions------------------------------
 
 function removeFromArray(event) {
     if (event.target.classList.contains('white-x-icon')){
         for(var i = 0; i < ideaBoxArray.length; i++) {
             if( ideaBoxArray[i].id === parseInt(event.target.parentElement.parentElement.id)) {
-                ideaBoxArray.splice(i, 1)
-                console.log("remove from array", ideaBoxArray, ideaBoxArray.length)
-                // working !!
+                ideaBoxArray.splice(i, 1);
             }
         }
     }
 }
 
 function saveIdea() {
-    newIdeaBox = new Idea(titleInput.value, bodyInput.value)
-    ideaBoxArray.push(newIdeaBox)
-    bodyInputEnteredVar = false
-    titleInputEnteredVar = false
-    console.log("add array", ideaBoxArray, ideaBoxArray.length)
-    // working!!
-    return newIdeaBox
+    newIdeaBox = new Idea(titleInput.value, bodyInput.value);
+    ideaBoxArray.push(newIdeaBox);
+    bodyInputEnteredVar = false;
+    titleInputEnteredVar = false;
+    return newIdeaBox;
 }
 
 function clearForm() {
-    titleInput.value = " " ;
-    bodyInput.value = " "
+    titleInput.value = " ";
+    bodyInput.value = " ";
 }
 
 function renderIdeaBox() {
     cardGrid.innerHTML = "";
-    for (var i = 0; i < ideaBoxArray.length; i++){
-    if (ideaBoxArray[i].star === false) {
-    cardGrid.innerHTML += `
-            <div class="card-container new-card" id=${ideaBoxArray[i].id}>
-                <div class="card-header">
-                    <img class="white-star-icon" src="assets/star.svg">
-                    <img class="red-star-icon hidden" src="assets/star-active.svg">
-                    <img class="star white-x-icon" src="assets/delete.svg">
-                </div>
-                <h2 class="card-title">${ideaBoxArray[i].title}</h2>
-                <p class="card-body rendered-body">${ideaBoxArray[i].body}</p>
-                <div class="card-footer"></div>
-            </div>`
-    }
-if (ideaBoxArray[i].star) {
-    cardGrid.innerHTML += `
-            <div class="card-container new-card" id=${ideaBoxArray[i].id}>
-                <div class="card-header">
-                    <img class="white-star-icon hidden" src="assets/star.svg">
-                    <img class="red-star-icon" src="assets/star-active.svg">
-                    <img class="star white-x-icon" src="assets/delete.svg">
-                </div>
-                <h2 class="card-title">${ideaBoxArray[i].title}</h2>
-                <p class="card-body rendered-body">${ideaBoxArray[i].body}</p>
-                <div class="card-footer"></div>
-            </div>`
-}
+    for (var i = 0; i < ideaBoxArray.length; i++) {
+        if (ideaBoxArray[i].star === false) {
+            cardGrid.innerHTML += `
+                <div class="card-container new-card" id=${ideaBoxArray[i].id}>
+                    <div class="card-header">
+                        <img class="white-star-icon" src="assets/star.svg">
+                        <img class="red-star-icon hidden" src="assets/star-active.svg">
+                        <img class="star white-x-icon" src="assets/delete.svg">
+                    </div>
+                    <h2 class="card-title">${ideaBoxArray[i].title}</h2>
+                    <p class="card-body rendered-body">${ideaBoxArray[i].body}</p>
+                    <div class="card-footer"></div>
+                </div>`
+        }
+        if (ideaBoxArray[i].star) {
+            cardGrid.innerHTML += `
+                <div class="card-container new-card" id=${ideaBoxArray[i].id}>
+                    <div class="card-header">
+                        <img class="white-star-icon hidden" src="assets/star.svg">
+                        <img class="red-star-icon" src="assets/star-active.svg">
+                        <img class="star white-x-icon" src="assets/delete.svg">
+                    </div>
+                    <h2 class="card-title">${ideaBoxArray[i].title}</h2>
+                    <p class="card-body rendered-body">${ideaBoxArray[i].body}</p>
+                    <div class="card-footer"></div>
+                </div>`
+        }
     }
 }
 
@@ -118,42 +101,16 @@ function favoriteCard(event) {
     for (var i = 0; i < ideaBoxArray.length; i++) {
         if (event.target.classList.contains('white-star-icon')) {
             event.target.classList.toggle('hidden');
-            event.target.nextElementSibling.classList.toggle('hidden')
-            return
+            event.target.nextElementSibling.classList.toggle('hidden');
+            return;
         }
         if (event.target.classList.contains('red-star-icon')) {
             event.target.classList.toggle('hidden');
-            event.target.previousElementSibling.classList.toggle('hidden')
-            return
+            event.target.previousElementSibling.classList.toggle('hidden');
+            return;
         }
     }
 }
-
-// function saveFavorites() {
-//     for (var i = 0; i < ideaBoxArray.length; i++) {
-//         if (ideaBoxArray[i].star === true) {
-//             console.log("HEY")
-//             cardGrid.innerHTML += `
-//             <div class="card-container new-card" id=${ideaBoxArray[i].id}>
-//                 <div class="card-header">
-//                     <img class="white-star-icon hidden" src="assets/star.svg">
-//                     <img class="red-star-icon" src="assets/star-active.svg">
-//                     <img class="star white-x-icon" src="assets/delete.svg">
-//                 </div>
-//                 <h2 class="card-title">${ideaBoxArray[i].title}</h2>
-//                 <p class="card-body rendered-body">${ideaBoxArray[i].body}</p>
-//                 <div class="card-footer"></div>
-//             </div>`
-//             return
-//         }
-        
-//     }
-// }
-
-
-
-
-
    
 function disableSaveButton() {
     saveButton.disabled = true;
@@ -169,9 +126,9 @@ function enableSaveButton() {
 }
 
 function titleInputEntered() {
-    var titleInputLength = titleInput.value.trim().length
+    var titleInputLength = titleInput.value.trim().length;
     if (titleInputLength !== 0) {
-    titleInputEnteredVar = true;
+        titleInputEnteredVar = true;
     }
     else {
         titleInputEnteredVar = false;
@@ -179,9 +136,9 @@ function titleInputEntered() {
 }
 
 function bodyInputEntered() {
-    var bodyInputLength = bodyInput.value.trim().length
+    var bodyInputLength = bodyInput.value.trim().length;
     if (bodyInputLength !== 0) {
-    bodyInputEnteredVar = true;
+        bodyInputEnteredVar = true;
     }
     else {
         bodyInputEnteredVar = false; 
@@ -192,4 +149,4 @@ function deleteCard(event) {
     if(event.target.classList.contains(`white-x-icon`)) {
       event.target.closest('.new-card').remove();
     }
-  }
+}
