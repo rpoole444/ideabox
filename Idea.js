e9e17473507978ac5f03b4ec4aba2aeb56fa2048
Tristin Sorrells <Tristinsorrells1@gmail.com>
2022-11-07 17:02:34 -0600

    Refactor: delete console.log statements

--- Idea.js
+++ Idea.js
@@ -1,20 +1,18 @@
 class Idea {
         constructor(title, body) {
-                this.title = title
-                this.body = body
+                this.title = title;
+                this.body = body;
                 this.star = false;
                 this.id = Date.now();
         }
         updateIdea(event) {
                 for (var i = 0; i < ideaBoxArray.length; i++) {
-                        var grandparentIdToString = parseInt(event.target.parentElement.parentElement.id)
+                        var grandparentIdToString = parseInt(event.target.parentElement.parentElement.id);
                         if (grandparentIdToString === ideaBoxArray[i].id && event.target.classList.contains('white-star-icon')) {
-                                ideaBoxArray[i].star = true
-                                console.log("Starred", ideaBoxArray)
+                                ideaBoxArray[i].star = true;
                         }
                         else if (grandparentIdToString === ideaBoxArray[i].id && event.target.classList.contains('red-star-icon')) {
-                                ideaBoxArray[i].star = false
-                                console.log("Unstarred", ideaBoxArray)
+                                ideaBoxArray[i].star = false;
                         }
                 }
         }
