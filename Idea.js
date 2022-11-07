afcb7ef4fc089133624ff391fb4d546320f21ad3
Tristin Sorrells <Tristinsorrells1@gmail.com>
2022-11-07 16:59:18 -0600

    Refactor: fix formatting of our Idea class and method

--- Idea.js
+++ Idea.js
@@ -1,26 +1,24 @@
 class Idea {
-    constructor (title, body) {
-            this.title = title
-            this.body = body
-            this.star = false;
-            this.id = Date.now();
+        constructor(title, body) {
+                this.title = title
+                this.body = body
+                this.star = false;
+                this.id = Date.now();
         }
-updateIdea(event) {
-        for (var i = 0; i < ideaBoxArray.length; i++) {
-                var grandparentIdToString = parseInt(event.target.parentElement.parentElement.id)
-                if (grandparentIdToString === ideaBoxArray[i].id && event.target.classList.contains('white-star-icon')) {
-                        ideaBoxArray[i].star = true
-                         console.log("Starred", ideaBoxArray)
-        }
-                else if (grandparentIdToString === ideaBoxArray[i].id && event.target.classList.contains('red-star-icon')) {
-                        ideaBoxArray[i].star = false
-                        console.log("Unstarred", ideaBoxArray)
+        updateIdea(event) {
+                for (var i = 0; i < ideaBoxArray.length; i++) {
+                        var grandparentIdToString = parseInt(event.target.parentElement.parentElement.id)
+                        if (grandparentIdToString === ideaBoxArray[i].id && event.target.classList.contains('white-star-icon')) {
+                                ideaBoxArray[i].star = true
+                                console.log("Starred", ideaBoxArray)
+                        }
+                        else if (grandparentIdToString === ideaBoxArray[i].id && event.target.classList.contains('red-star-icon')) {
+                                ideaBoxArray[i].star = false
+                                console.log("Unstarred", ideaBoxArray)
+                        }
                 }
         }
-} 
-return
-}       
-
+}     
 
 
 
